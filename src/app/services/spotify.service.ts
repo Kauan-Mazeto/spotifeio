6541661f1492b08b39a756098bf5528901db0fd9
unknown <kauan.mazetorodrigues@gmail.com>
2025-08-19 22:30:50 -0300

    novo commit

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -10,7 +10,23 @@ export class SpotifyService {
     constructor() {}
 
     async obterUrlLogin(): Promise<string> {
-        
+        const codigoAleatorio = await this.gerarCodigoAleatorio();
         const authPoint = `${SpotifyConfiguration.authEndpoint}?`;
+        const clientId = `client_id=${SpotifyConfiguration.clientId}&`;
+        const urlRedirection = `redirect_url=${SpotifyConfiguration.redirectUrl}&`;
+        const scopes = `scope=${SpotifyConfiguration.scopes.join('%20')}`;
+        const codeChallengeMethod = 'code_challenge_method=S256';
+        const codeChallangeParam = 'code_challenge' + codigoAleatorio + '&'; 
+        const responseType = 'response_type=code';
+        
+        return `${authPoint}${clientId}${urlRedirection}${scopes}${codeChallengeMethod}${codeChallangeParam}${responseType}`;
+
+    }
+
+    async gerarCodigoAleatorio(): Promise<string> {
+        const codigoVerificador = this.gerarCodigoVerificador(128);
+        const codeChellange = await this.gerarHash(codigoVerificador);
+
+        return codeChellange;
     }
 }
